f32fa22e6edb001e399ae938722510b25ef95b09
Evgeniy Ladyko <evgeniy.ladyko@gmail.com>
2019-04-27 01:39:59 +0400

    Added NarcissisticNumbers solution (https://www.codewars.com/kata/5287e858c6b5a9678200083c)

---
+++ Codewars.Solutions.Test/NarcissisticNumbersTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codewars.Solutions.Test
+{
+    [TestClass]
+    public class NarcissisticNumbersTests
+    {
+        [DataTestMethod()]
+        [DataRow(1)]
+        [DataRow(371)]
+        [DataRow(153)]
+        [DataRow(1634)]
+        public void NarcissisticTest(int value)
+        {
+            var solver = new NarcissisticNumbers();
+            var result = solver.Narcissistic(value);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Codewars.Solutions/NarcissisticNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codewars.Solutions
+{
+    public class NarcissisticNumbers
+    {
+        public bool Narcissistic(int value)
+        {
+            var str = value.ToString();
+            var n = str.Length;
+            int sum = 0;
+
+            foreach (var c in str)
+            {
+                sum += (int)Math.Pow(int.Parse(c.ToString()), n);
+            }
+
+            return value == sum;
+        }
+    }
+}
